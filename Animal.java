5021364a8db113b5925182dc8ede32f3863958c9
4NC4 <4NC4@users.noreply.github.com>
2020-08-07 14:31:43 +0300

    Implemented dog fetaure

--- Animal.java
+++ Animal.java
@@ -1,5 +1,14 @@
 public class Animal {
 	public static void main(String[] args) {
 		System.out.print("I am a " + args[0]);
+		System.out.print("and I say " + saySomething(args[0]));
+	}
+	
+	private static String saySomething(String animal) {
+		if(animal.equals("dog")) {
+			return "woof";
+		} else {
+			return "????";
+		}
 	}
 }
